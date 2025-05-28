9e7174a6e3e19f2dd4683d46744bb8959c17e4d5
a-murayama0625 <127582200+a-murayama0625@users.noreply.github.com>
2025-05-28 12:38:53 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,7 @@
 # Import python packages
+# from snowflake.snowpark.context import get_active_session
+
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -22,8 +23,8 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
